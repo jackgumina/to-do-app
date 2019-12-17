f106f1e9e5ddd485b88e30c8d0749f607ae92490
Jack <jackgumina@gmail.com>
2019-12-17 14:28:37 -0600

    Checkpoint 12.12 submission

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 function onReady() {
-  const addToDoForm = document.getElementByID('addToDoForm');
-  const newToDoText = document.getElementByID('newToDoText');
-  const toDoList = document.getElementByID('toDoList');
+  const addToDoForm = document.getElementById('addToDoForm');
+  const newToDoText = document.getElementById('newToDoText');
+  const toDoList = document.getElementById('toDoList');
 
   addToDoForm.addEventListener('submit', event => {
 
@@ -12,18 +12,31 @@ function onReady() {
 
     // create a new li
     let newLi = document.createElement('li');
+    newLi.className += "mdl-list__item";
 
     // create a new input
     let checkbox = document.createElement('input');
+    checkbox.className += "mdl-checkbox__input";
 
     //set the input's type to checkbox
     checkbox.type = "checkbox";
+
+    // create a new button
+    let deleteBtn = document.createElement('button');
+    deleteBtn.textContent = "Delete";
+    deleteBtn.className += "mdl-button mdl-button--colored mdl-js-button mdl-js-ripple-effect";
+
+    deleteBtn.addEventListener('click', function(event){
+      toDoList.removeChild(this.parentElement);
+    })
 
     // set the title
     newLi.textContent = title;
 
     // attach the checkbox to the li
     newLi.appendChild(checkbox);
+
+    newLi.appendChild(deleteBtn);
 
     // attach the li to the ul
     toDoList.appendChild(newLi);
