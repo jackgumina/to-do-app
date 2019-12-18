df510549fe0af05b6167c51416ce786ee6ef3225
Jack <jackgumina@gmail.com>
2019-12-18 11:11:50 -0600

    assignment 13 completed

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 function onReady() {
-  const toDos = [];
+  let toDos = [];
+  let id = 0;
   const addToDoForm = document.getElementById('addToDoForm');
 
   function createNewToDo() {
@@ -8,8 +9,11 @@ function onReady() {
 
     toDos.push({
       title: newToDoText.value,
-      complete: false
+      complete: false,
+      id: id
     });
+
+    id++;
 
     newToDoText.value = '';
 
@@ -26,10 +30,22 @@ function onReady() {
       const checkbox = document.createElement('input');
       checkbox.type = "checkbox";
 
+      const deleteBtn = document.createElement('button');
+      deleteBtn.textContent = 'Delete';
+
+      deleteBtn.addEventListener('click', event => {
+        toDos = toDos.filter(function(item) {
+          return item.id !== toDo.id;
+        })
+
+        renderTheUI();
+      });
+
       newLi.textContent = toDo.title;
 
       toDoList.appendChild(newLi);
       newLi.appendChild(checkbox);
+      newLi.appendChild(deleteBtn);
     });
   }
 
